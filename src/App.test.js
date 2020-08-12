d2b5ffe91482678c7869f493ada1fa82d203e5fc
Juan Ortiz <juanfortizd@gmail.com>
2020-08-12 16:31:30 -0500

    add example test to verify Codeship check

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import { render } from '@testing-library/react';
 import App from './App';
 
+function sum(a, b) {
+  return a + b;
+}
+
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
@@ -12,4 +16,8 @@ test('renders learn react link', () => {
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
 });
