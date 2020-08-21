2c261178b35854ca5d9b983768bcfaea08cdcb5a
Juan Ortiz <juanfortizd@gmail.com>
2020-08-21 13:57:26 -0500

    iterate over translation options when rendering form

--- src/components/App.js
+++ src/components/App.js
@@ -17,18 +17,20 @@ class App extends React.Component {
 
     return (
       <div className="app">
-        { alert &&
-          <CustomAlert alert={alert} {...myProps} />
-        }
-        <Header {...myProps}/>
-        <main style={{'height': `calc(100vh - ${this.headerHeight})`}}>
-          <Switch>
-            <Route path='/' exact render={ props => <Redirect to="/start/zip" /> } />
-            <Route path='/start/zip' render={ props => <StartZipContainer {...props} {...myProps} /> }/>
-            <Route path='/start/info' render={ props => <StartInfo {...props} {...myProps} /> }/>
-            <Route path='/:page' render={ props => <Redirect to="/start/zip" /> }/>
-          </Switch>
-        </main>
+        <React.Suspense fallback="loading">
+          { alert &&
+            <CustomAlert alert={alert} {...myProps} />
+          }
+          <Header {...myProps}/>
+          <main style={{'height': `calc(100vh - ${this.headerHeight})`}}>
+            <Switch>
+              <Route path='/' exact render={ props => <Redirect to="/start/zip" /> } />
+              <Route path='/start/zip' render={ props => <StartZipContainer {...props} {...myProps} /> }/>
+              <Route path='/start/info' render={ props => <StartInfo {...props} {...myProps} /> }/>
+              <Route path='/:page' render={ props => <Redirect to="/start/zip" /> }/>
+            </Switch>
+          </main>
+        </React.Suspense>
       </div>
     );
   }
--- src/components/main/StartInfo.js
+++ src/components/main/StartInfo.js
@@ -1,23 +1,66 @@
 import React from 'react';
 import FormContainer from '../shared/FormContainer';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import { withTranslation } from 'react-i18next';
 import { ReactComponent as ShieldLogo } from '../../images/no-spam-shield.svg';
-import { progressBarStatus } from '../../constants/progress-bar-percentages'
+import { ProgressBarStatus } from '../../constants/progress-bar-percentages'
+import Radio from '../forms/Radio'
+
 
 class StartInfo extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { carInsurance: undefined, homeOwnership: undefined }
+  }
+
   componentDidMount() {
     const { setProgress } = this.props
-    setProgress(progressBarStatus.DRIVERS)
+    setProgress(ProgressBarStatus.DRIVERS)
   }
 
   render() {
     const { t } = this.props
-
+    const changeHomeOwnership = event => this.setState({ homeOwnership: event.target.value })
+    const changeCarInsurance  = event => this.setState({ carInsurance:  event.target.value })
+console.log(t('info.fields.home.options'))
     return (
       <React.Fragment>
         <FormContainer bootstrapProperties={{md: {span: 6, offset: 3}}}>
           <h2 className="mb-5 font-weight-bold ">{t('info.title')}</h2>
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Label>{t('info.fields.home.label')}</Form.Label>
+            <div className=''>
+              { t('info.fields.home.options').map((item, index)=>
+
+              <Radio type={'radio'} id={'info-home-own'}
+                     label={t('info.fields.home.options.own.label')}
+                     value={t('info.fields.home.options.own.value')}
+                     selected={this.state.homeOwnership === t('info.fields.home.options.own.value')}
+                     onChange={changeHomeOwnership} inline={true}/>
+
+              ) }
+            </div>
+            <Form.Label>{t('info.fields.car.label')}</Form.Label>
+            <div className=''>
+              <Radio
+                type={'radio'} id={'info-car-true'}
+                label={t('info.fields.car.options.true.label')}
+                value={t('info.fields.car.options.true.value')}
+                selected={this.state.carInsurance === t('info.fields.car.options.true.value')}
+                onChange={changeCarInsurance} inline={true}/>
+              <Radio
+                type={'radio'} id={'info-car-false'}
+                label={t('info.fields.car.options.false.label')}
+                value={t('info.fields.car.options.false.value')}
+                selected={this.state.carInsurance === t('info.fields.car.options.false.value')}
+                onChange={changeCarInsurance} inline={true}/>
+            </div>
+            <div className='w-75 mx-auto'>
+              <Button size='lg' variant="primary" type="submit" block disabled={{/*!this.state.enableSubmit*/}}>
+                {t('info.submit')}
+              </Button>
+            </div>
+          </Form>
         </FormContainer>
         <Container>
           <Row>
