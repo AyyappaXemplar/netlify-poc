8207d403a1d433c094d90267b74d959d327ab774
Juan Ortiz <juanfortizd@gmail.com>
2020-09-04 12:12:53 -0500

    add link to edit vehicle to Vehicle component

--- src/components/shared/Vehicle.js
+++ src/components/shared/Vehicle.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import history from '../../history'
 import QuoteItemCard from './QuoteItemCard'
 import { withTranslation } from 'react-i18next';
 import { ReactComponent as SampleIcon } from '../../images/sample.svg';
@@ -6,7 +7,17 @@ import { ReactComponent as PencilIcon } from '../../images/pencil.svg'
 import { ReactComponent as TrashIcon } from '../../images/trash.svg'
 
 class Vehicle extends React.Component {
-  onDelete() {
+  constructor(props) {
+    super(props)
+    this.editVehicle = this.editVehicle.bind(this)
+    this.deleteVehicle = this.deleteVehicle.bind(this)
+  }
+  editVehicle() {
+    const { vehicle } = this.props
+    history.push(`/vehicles/${vehicle.id}/edit`)
+  }
+
+  deleteVehicle() {
     const { deleteVehicle, t, vehicle } = this.props
     let confirmed = window.confirm(t('vehiclesIndex:fields.vehicle.deleteConfirm'))
 
@@ -22,13 +33,12 @@ class Vehicle extends React.Component {
     const icon = <SampleIcon/>
     const title = `${year} ${manufacturer} ${model} ${trim}`
     const body = t(`fields.use.useCode.${use_code}.label`)
-    const onDelete = this.onDelete.bind(this)
 
     return (
       <QuoteItemCard icon={icon} title={title} body={body}>
         <div className='actions text-med-light'>
-          <PencilIcon className="mr-3"/>
-          <TrashIcon onClick={onDelete}/>
+          <PencilIcon className="mr-3" onClick={this.editVehicle}/>
+          <TrashIcon onClick={this.deleteVehicle}/>
         </div>
       </QuoteItemCard>
     )
