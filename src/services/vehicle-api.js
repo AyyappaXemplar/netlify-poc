cfa3b4cb2204631fe8ad40a1f181f9e9609f4ed4
Juan Ortiz <juanfortizd@gmail.com>
2020-09-15 12:33:02 -0500

    fix url string

--- src/services/vehicle-api.js
+++ src/services/vehicle-api.js
@@ -4,7 +4,7 @@ export default class VehicleOptionsApi {
   static apiUrl = `${process.env.REACT_APP_API_BASE_URL}/${process.env.REACT_APP_API_NAMESPACE}`
 
   static manufacturer(year) {
-    const url = `${this.apiUrl}/vehicles/${year}/make/`
+    const url = `${this.apiUrl}/vehicles/${year}/makes/`
     return Axios.get(url)
   }
 
