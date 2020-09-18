1d60f98ec4d3623731489881f34cfe672611d565
Juan Ortiz <juanfortizd@gmail.com>
2020-09-18 09:48:52 -0500

    add quote screen structure and front matter for complete quote

--- src/constants/quote-screen-structure.js
+++ src/constants/quote-screen-structure.js
@@ -1,23 +1,30 @@
+import { ProgressBarStatus } from './progress-bar-percentages';
+
 const quoteScreenStructure = {
   vehicles: {
     saveUrl: '/drivers/new',
     itemsBeforeButton: ['vehicles'],
-    itemsAfterButton:  ['discounts']
+    itemsAfterButton:  ['discounts'],
+    progressBarStatus: ProgressBarStatus.VEHICLES
   },
   drivers: {
     saveUrl: '/quote/discounts',
     itemsBeforeButton: ['drivers'],
-    itemsAfterButton:  ['vehicles', 'discounts']
+    itemsAfterButton:  ['vehicles', 'discounts'],
+    progressBarStatus: ProgressBarStatus.DRIVERS
   },
   discounts: {
     saveUrl: '/quote/complete',
     itemsBeforeButton: ['discounts'],
-    itemsAfterButton:  ['vehicles', 'drivers']
+    itemsAfterButton:  ['vehicles', 'drivers'],
+    progressBarStatus: ProgressBarStatus.DISCOUNTS
   },
   complete: {
-    saveUrl: '/quote',
+    saveUrl: '/quote/submit',
     itemsBeforeButton: ['vehicles', 'drivers', 'discounts'],
-    itemsAfterButton:  []
+    itemsAfterButton:  [],
+    progressBarStatus: ProgressBarStatus.DISCOUNTS,
+    showWarnings: true
   }
 }
 
