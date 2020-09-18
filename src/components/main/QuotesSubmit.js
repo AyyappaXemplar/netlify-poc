fe4533e09d94847564a245516a70476cd6f3a30d
Juan Ortiz <juanfortizd@gmail.com>
2020-09-18 14:55:45 -0500

    correct class proprety name

--- src/components/main/QuotesSubmit.js
+++ src/components/main/QuotesSubmit.js
@@ -42,7 +42,7 @@ class QuotesSubmit extends React.Component {
         <TitleRow title={t('quotes:submit.title')}></TitleRow>
         {
           showSpinner &&
-          <div class="text-center">
+          <div className="text-center">
             <div className="spinner-border"role="status">
               <span className="sr-only">Loading...</span>
             </div>
