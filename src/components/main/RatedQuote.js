b9ebfa35c6ef10436433041d04b3d7acab273e4a
Juan Ortiz <juanfortizd@gmail.com>
2020-09-24 17:32:02 -0500

    extract pricing tabs into its own component

--- src/components/main/RatedQuote.js
+++ src/components/main/RatedQuote.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { withTranslation } from 'react-i18next';
 // import history from "../../history";
-import TitleRow from "../shared/TitleRow";
 import RatedQuoteDriver from "../shared/RatedQuoteDriver";
 import RatedQuoteVehicle from "../shared/RatedQuoteVehicle";
-import { Row, Col, Tab, Tabs } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Row, Col } from 'react-bootstrap';
 import RatedQuote from '../../server/ratedQuote.js';
-import QuoteCoverageStrength from '../shared/QuoteCoverageStrength';
-import QuoteCoveragePricing from '../shared/QuoteCoveragePricing';
+import PricingTabs from '../shared/PricingTabs';
 
 class QuotesRate extends React.Component {
   render() {
@@ -35,31 +32,6 @@ class QuotesRate extends React.Component {
       )
     })
 
-    const priceTabs = quote.rate.payment_options.map((option, index) => {
-      let price = option.policy_premium
-      let title = <div className="text-center p-2">{option.plan_description}</div>
-
-      return (
-        <Tab eventKey={option.plan_description} key={option.plan_description} title={title}>
-          <div className="rated-quote-item-card p-5">
-            <div className="title mb-3">Quote #{quote.id}</div>
-            <div className="d-flex price-container mb-5">
-              <p className="price-container__price quote-price display-1 mb-0">
-                <sup className="price-container__dollar">$</sup>
-                {price}
-              </p>
-              <span className="price-container__text align-self-end text-med-dark ml-1">per<br/> month</span>
-            </div>
-            <QuoteCoverageStrength strength={1}/>
-            <QuoteCoveragePricing  strength={2}/>
-            <div className="mx-auto mb-5">
-              <Link className="rounded-pill btn btn-primary btn-block btn-lg" to={'/#'}>Buy Online</Link>
-            </div>
-          </div>
-        </Tab>
-      )
-    })
-
     return (
       <>
         <Row>
@@ -67,11 +39,7 @@ class QuotesRate extends React.Component {
             <h1>{t('quotes:rate.title')}</h1>
           </Col>
           <Col lg={ {span: 5} }>
-            <div className='bg-white shadow-sm'>
-              <Tabs defaultActiveKey={quote.rate.payment_options[0].plan_description}>
-                { priceTabs }
-              </Tabs>
-            </div>
+            <PricingTabs quote={quote}/>
           </Col>
         </Row>
 
