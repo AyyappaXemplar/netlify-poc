2675be8d05733750821d066892a1fb448876d99c
Juan Ortiz <juanfortizd@gmail.com>
2020-09-29 17:45:36 -0500

    move routes to external file

---
+++ src/routes.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import QuotesNew from './containers/QuotesNew';
+import QuotesEdit from './containers/QuotesEdit';
+import QuotesNotCovered from './components/main/QuotesNotCovered';
+import VehiclesNew from './containers/VehiclesNew';
+import VehiclesEdit from './containers/VehiclesEdit';
+import Quotes from './containers/Quotes';
+import QuotesSubmit from './containers/QuotesSubmit';
+import RatedQuote from './containers/RatedQuote';
+import DriversNew from './containers/DriversNew';
+import DriversEdit from './containers/DriversEdit';
+
+const routes = [
+  { path:'/', exact: true, main: (props) => <Redirect to="/quotes/new" /> },
+  { path:'/quotes/new',    main: (props) => <QuotesNew   {...props} />, showProgress: true },
+  { path:'/quotes/edit',   main: (props) => <QuotesEdit  {...props} />, showProgress: true },
+  { path:'/quotes/submit', main: (props) => <QuotesSubmit {...props} />, showProgress: true },
+  { path:'/quotes/rated',  main: (props) => <RatedQuote   {...props} /> },
+  { path:'/quotes/not-covered', main: (props) => <QuotesNotCovered  {...props} />, showProgress: true },
+  { path:'/quotes/:resource(vehicles|drivers|discounts|review)', main: (props) => <Quotes {...props} /> },
+  { path:'/vehicles/new',  main: (props) => <VehiclesNew {...props} />, showProgress: true },
+  { path:'/vehicles/:vehicleId/edit', main: (props) => <VehiclesEdit      {...props} />, showProgress: true },
+  { path:'/drivers/new',   main: (props) => <DriversNew   {...props} />,          showProgress: true },
+  { path:'/drivers/:driverId/edit' , main: (props) => <DriversEdit {...props} />, showProgress: true },
+  { path:'/:page',         main: (props) => <Redirect to="/quotes/new" /> }
+];
+
+export default routes
