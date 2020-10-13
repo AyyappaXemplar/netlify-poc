95aa73787b19ffe879ea78effe1b83a41fd8461a
Juan Ortiz <juanfortizd@gmail.com>
2020-10-13 13:11:27 -0500

    get rates after updating vehicle coverages

--- src/actions/vehicles.js
+++ src/actions/vehicles.js
@@ -1,6 +1,6 @@
 import Axios from 'axios';
 import * as types from '../constants/vehicle-action-types';
-import { getQuote } from './quotes'
+import { rateQuote } from './quotes'
 
 const apiBase = process.env.REACT_APP_API_BASE_URL
 const namespace = process.env.REACT_APP_API_NAMESPACE
@@ -56,7 +56,7 @@ export const updateVehicleCoverages = (vehicleId, coverageLevel) => {
 
     return Axios.patch(`${apiBase}/${namespace}/quotes/${quoteId}/vehicles/${vehicleId}`, vehicleParams)
       .then(response => {
-        dispatch(getQuote());
+        dispatch(rateQuote());
       }).catch(error => {
         dispatch(receiveUpdateVehicleResponse('error'));
       })
