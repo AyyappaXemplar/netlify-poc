7b30e254667040ae0e17011afe0061ee94ec9250
Juan Ortiz <juanfortizd@gmail.com>
2020-10-15 12:18:48 -0500

    move star icon to its own component

--- src/components/main/Rate.js
+++ src/components/main/Rate.js
@@ -6,6 +6,7 @@ import RatedQuoteVehicle from "../shared/RatedQuoteVehicle";
 import { Row, Col } from 'react-bootstrap';
 import PricingTabs from '../shared/PricingTabs';
 import image from '../../images/FCIC-Logo.png'
+import { ReactComponent as StarIcon } from '../../images/star.svg'
 import { Link } from 'react-router-dom'
 
 function Rate(props) {
@@ -63,11 +64,7 @@ function Rate(props) {
             </div>
             <div className="mb-3 d-flex align-items-center">
               <div className="text-warning d-inline-block mr-2 mb-1">
-                {[1,2,3,4,5].map(number => (
-                  <svg key={number} fill="currentColor" width="1em" height="1em" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-                  </svg>
-                ))}
+                {[1,2,3,4,5].map(number => <StarIcon/>)}
               </div>
               <span>9.5/10</span>
             </div>
