95fe9cbf883300490beb136794b59fbef821f78b
Juan Ortiz <juanfortizd@gmail.com>
2020-10-16 10:48:16 -0500

    add dynamic rate price

--- src/components/main/RatesCompare.js
+++ src/components/main/RatesCompare.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { withTranslation } from 'react-i18next';
-import { Row, Col, } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import QuoteCoverageStrength from '../shared/QuoteCoverageStrength';
 import QuoteCoveragePricing from '../shared/QuoteCoveragePricing';
 import { Link } from 'react-router-dom';
-// import { Link } from 'react-router-dom'
+import { monthlyPaymentOption, priceDisplay, payInFullOption } from '../../services/payment-options'
 
-function RatesCompare(props) {
-  // const { t } = props;
-  let rates = useSelector(state => state.data.rates)
+function RatesCompare({ t }) {
+  const [payInFull, setPayInFull] = useState(true)
+
+  const rates = useSelector(state => state.data.rates)
 
   return (
     <Row>
       <Col xs={12} lg={{offset: 2, span: 8}}>
         <Row className="mt-5 justify-content-center">
           { rates.map((rate, index) =>
-            <Col xs={12} md={6} className='mb-4'>
-              <div key={index} className="bg-white rounded px-4 py-5">
+
+            <Col xs={12} md={6} className='mb-4' key={index}>
+              <div className="bg-white rounded px-4 py-5">
                 <h3>Rater Company {index}</h3>
                 <p className='text-med-dark'>
                   Rater company {index} text adipisicing exercitation adipisicing reprehenderit
@@ -28,7 +30,7 @@ function RatesCompare(props) {
                   <div className="d-flex price-container">
                     <p className="price-container__price mb-0">
                       <sup className="price-container__dollar">$</sup>
-                      50
+                      {priceDisplay(monthlyPaymentOption(rate))}
                     </p>
                     <span className="price-container__text align-self-end text-med-dark ml-1">per<br/> month</span>
                   </div>
@@ -40,7 +42,7 @@ function RatesCompare(props) {
                   <QuoteCoveragePricing strength='GOOD'/>
                 </div>
                 <div className="w-75 mx-auto">
-                  <Link to={`/quotes/rates?index=${index}`} className="rounded-pill btn btn-primary btn-block btn-lg">
+                  <Link to={`/rates?index=${index}`} className="rounded-pill btn btn-primary btn-block btn-lg">
                     Select Coverage
                   </Link>
                 </div>
--- src/routes.js
+++ src/routes.js
@@ -6,8 +6,7 @@ import QuotesNotCovered from './components/main/QuotesNotCovered';
 import VehiclesNew from './containers/VehiclesNew';
 import VehiclesEdit from './containers/VehiclesEdit';
 import Quotes from './components/main/Quotes';
-import QuotesSubmit from './components/main/QuotesSubmit';
-import Rate from './containers/Rate';
+import Rate from './components/main/Rate';
 import RatesCompare from './components/main/RatesCompare';
 import DriversNew from './containers/DriversNew';
 import DriversEdit from './containers/DriversEdit';
@@ -16,15 +15,14 @@ const routes = [
   { path:'/', exact: true, main: (props) => <Redirect to="/quotes/new" /> },
   { path:'/quotes/new',    main: (props) => <QuotesNew   {...props} />},
   { path:'/quotes/edit',   main: (props) => <QuotesEdit  {...props} />},
-  { path:'/quotes/submit', main: (props) => <QuotesSubmit {...props} /> },
-  { path:'/quotes/rates/compare',  main: (props) => <RatesCompare {...props} /> },
-  { path:'/quotes/rates',  main: (props) => <Rate {...props} /> },
   { path:'/quotes/not-covered', main: (props) => <QuotesNotCovered  {...props} />},
   { path:'/quotes/:resource(vehicles|drivers|discounts|review)', main: (props) => <Quotes {...props} />},
   { path:'/vehicles/new',  main: (props) => <VehiclesNew {...props} />},
   { path:'/vehicles/:vehicleId/edit', main: (props) => <VehiclesEdit {...props} />},
   { path:'/drivers/new',   main: (props) => <DriversNew   {...props} />},
   { path:'/drivers/:driverId/edit' , main: (props) => <DriversEdit {...props} />},
+  { path:'/rates/compare',  main: (props) => <RatesCompare {...props} /> },
+  { path:'/rates',  main: (props) => <Rate {...props} /> },
   { path:'/:page',         main: (props) => <Redirect to="/quotes/new" /> }
 ];
 
