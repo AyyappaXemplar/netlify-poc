533222f1f17deed0b47bda9661b0f05f51fdc38f
Juan Ortiz <juanfortizd@gmail.com>
2020-10-29 13:59:05 -0500

    add missing dependency

--- src/components/forms/DriverForm.js
+++ src/components/forms/DriverForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Col, Form, Button } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import { withTranslation } from 'react-i18next';
 import FormContainer from '../shared/FormContainer';
 import BadgeText from '../shared/BadgeText';
