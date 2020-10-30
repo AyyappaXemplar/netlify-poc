453192613ed33099118a46c7f620a13c63ea8cf1
John Koht <john@kohactive.com>
2020-10-29 23:40:06 -0500

    fix driver icon selection

--- src/services/driver-icon.js
+++ src/services/driver-icon.js
@@ -1,18 +1,18 @@
 import { ReactComponent as AdultFemale } from '../images/adult-female.svg';
-import { ReactComponent as AdultMale } from '../images/adult-male.svg';
+import { ReactComponent as AdultMale }   from '../images/adult-male.svg';
 import { ReactComponent as YoungFemale } from '../images/young-female.svg';
-import { ReactComponent as YoungMale } from '../images/young-male.svg';
+import { ReactComponent as YoungMale }   from '../images/young-male.svg';
+import { dateToAge }                     from '../services/driver-age'
 
 const DriverIcons = {
   AdultFemale: AdultFemale,
-  AdultMale: AdultMale,
+  AdultMale:   AdultMale,
   YoungFemale: YoungFemale,
-  YoungMale: YoungMale
+  YoungMale:   YoungMale
 }
 
-
 export default function getDriverIcon(driver) {
-  const age = driver.birthday > 21 ? 'Adult' : 'Young'
+  const age = dateToAge(driver.birthday) > 21 ? 'Adult' : 'Young'
   const gender = driver.gender === 'male' ? 'Male' : 'Female'
   return DriverIcons[`${age}${gender}`]
 }
