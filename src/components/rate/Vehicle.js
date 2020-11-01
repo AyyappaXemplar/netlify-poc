981be2562092d5471e894a018f64e276b93312bd
Juan Ortiz <juanfortizd@gmail.com>
2020-11-01 13:01:15 -0600

    extract vehicle coverages into its own component

--- src/components/rate/Vehicle.js
+++ src/components/rate/Vehicle.js
@@ -1,6 +1,6 @@
-import React                          from 'react';
-import { useSelector, useDispatch   } from 'react-redux';
-import { withTranslation }            from 'react-i18next';
+import React               from 'react';
+import { useDispatch   }   from 'react-redux';
+import { withTranslation } from 'react-i18next';
 
 import { deleteVehicle }  from '../../actions/vehicles'
 import history            from '../../history';
@@ -9,71 +9,19 @@ import { formatMoney }    from '../../services/payment-options'
 
 import { ReactComponent as PencilIcon } from '../../images/pencil.svg'
 import { ReactComponent as TrashIcon }  from '../../images/trash.svg'
-import { ReactComponent as CheckIcon }  from '../../images/check-circle-fill.svg';
 
 import CoverageStrength        from '../shared/CoverageStrength';
 import CoveragePricing         from '../shared/CoveragePricing';
-import DashIcon                from '../shared/DashCircle';
+import VehicleCoverages        from './VehicleCoverages';
 import VehicleCoverageSelector from './VehicleCoverageSelector';
 
 function RatedQuoteVehicle({ vehicle, t }) {
   const dispatch    = useDispatch()
-  const coverages   = useSelector(state => state.data.coverages)
-
-  const coverageValues = coverage => {
-    return (
-      coverage.limits.map(limit => {
-        // Divide by 100 to go from cents to dollars
-        let rounded = Math.round(limit.amount)/100;
-
-        // If it's smaller than 1000, we'll want to
-        // display as a number like $500 or $1,000.
-        if (rounded <= 1000) {
-          return `$${formatMoney(rounded)}`
-        } else {
-          rounded = Math.round(limit.amount)/100000;
-          return `$${rounded}K`
-        }
-      }).join(' / ')
-    )
-  }
-
-  const vehicleCoverage = item => {
-    let values = item.included ? coverageValues(item.coverage) : "N/A"
-    let icon = item.included ? <CheckIcon className='text-success'/> :
-                               <DashIcon circleFill="var(--primary)" rectFill="white"/>
-
-    return (
-      <div key={item.coverage.type} className="rate-item-card__attribute d-flex justify-content-between">
-        <div className='title d-flex align-items-center'>
-          {icon}
-          {item.coverage.description}
-        </div>
-        <div className='value text-capitalize'>{values}</div>
-      </div>
-    )
-  }
 
   const onDeleteVehicle = () => {
     let confirmed = window.confirm(t('quotes:fields.vehicle.deleteConfirm'))
 
     if (confirmed) dispatch(deleteVehicle(id))
-  }
-
-  const coverageDisplay = () => {
-    const { coverages: vehicleCoverages } = vehicle
-
-    const allCoverages = coverages.groupedByType.BETTER
-    const displayedCoverages = vehicleCoverages.map(item => ({coverage: item, included: true }))
-
-    // insert coverages not included in the array
-    allCoverages.forEach(item => {
-      let included = vehicleCoverages.find(cov => cov.type === item.type)
-
-      if (!included) displayedCoverages.push({ coverage: item, included: false })
-    })
-
-    return displayedCoverages.map(item => vehicleCoverage(item))
   }
 
   const { manufacturer, model, year, use_code,
@@ -118,7 +66,7 @@ function RatedQuoteVehicle({ vehicle, t }) {
         </div>
       </div>
 
-      { coverageDisplay() }
+      <VehicleCoverages vehicle={vehicle}/>
 
     </div>
   )
