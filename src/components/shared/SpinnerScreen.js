b08554857c2a96454fcc5ee67298a03808bacef1
John Koht <john@kohactive.com>
2020-11-06 10:41:36 -0600

    remove unused packages from spinner component

--- src/components/shared/SpinnerScreen.js
+++ src/components/shared/SpinnerScreen.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Row, Col } from 'react-bootstrap'
 
 function SpinnerScreen({ title, t }) {
   const [displayedTitle, setDisplayedTitle] = useState('Loading')
