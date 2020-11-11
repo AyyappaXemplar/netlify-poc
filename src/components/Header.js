25358cae9599a1b5cd9496efe76ec7d9c4475851
kuladesign <kkuladesign@gmail.com>
2020-11-11 15:37:09 -0600

    freshchat

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import { Container, Row, Col } from 'react-bootstrap';
-import { ReactComponent as Logo } from '../images/logo.svg';
-import { withTranslation } from 'react-i18next';
-import progressBarRoutes from '../progress-bar-routes'
-import PhoneNumberLink from './shared/PhoneNumberLink'
+import React from "react"
+import { Route } from "react-router-dom"
+import { Container, Row, Col } from "react-bootstrap"
+import { ReactComponent as Logo } from "../images/logo.svg"
+import { withTranslation } from "react-i18next"
+import progressBarRoutes from "../progress-bar-routes"
+import PhoneNumberLink from "./shared/PhoneNumberLink"
+import FreshChat from "react-freshchat"
 
 class Header extends React.Component {
   progressBar() {
@@ -14,29 +15,60 @@ class Header extends React.Component {
   }
 
   render() {
-    const { t } = this.props;
+    const { t } = this.props
     const progressBar = this.progressBar()
+    const freshChatToken = "c652d506-4d34-4618-a610-b6929299ed2a"
 
     return (
-      <Container className='header-container'>
-        <Row className='align-items-center header-row'>
-          <Col xs={12} sm={6} lg={3} className="text-center text-sm-left my-3 my-sm-0">
-            <Logo className='logo'/>
+      <Container className="header-container">
+        <Row className="align-items-center header-row">
+          <Col
+            xs={12}
+            sm={6}
+            lg={3}
+            className="text-center text-sm-left my-3 my-sm-0"
+          >
+            <Logo className="logo" />
           </Col>
-          <Col xs={{order: 3, span: 12 }} lg={{ order: 0, span: 6 }}>
-            { progressBar }
+          <Col xs={{ order: 3, span: 12 }} lg={{ order: 0, span: 6 }}>
+            {progressBar}
           </Col>
-          <Col xs={12} sm={6} lg={3} className="header-contact text-center text-sm-right">
-            <small className='mb-0'>{t('header.title')}</small>
+          <Col
+            xs={12}
+            sm={6}
+            lg={3}
+            className="header-contact text-center text-sm-right"
+          >
+            <small className="mb-0">{t("header.title")}</small>
 
             <p class="h5 mb-0">
-              <PhoneNumberLink number={t('header.phoneNumber')} classes="text-dark" />
+              <PhoneNumberLink
+                number={t("header.phoneNumber")}
+                classes="text-dark"
+              />
             </p>
           </Col>
         </Row>
+        <div>
+          <FreshChat
+        token={freshChatToken}
+            email="user@email.com"
+            first_name="..."
+            onInit={(widget) => {
+              /* Use `widget` instead of `window.fcWidget`
+            widget.user.setProperties({
+              email: user.email,
+              first_name: user.firstName,
+              last_name: user.lastName,
+              phone: user.phoneNumber,
+            })
+          */
+            }}
+          />
+        </div>
       </Container>
-    );
+    )
   }
 }
 
-export default withTranslation(['common'])(Header);
+export default withTranslation(["common"])(Header)
