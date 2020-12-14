40fdd3833a358ee5c9bbdbb592a92be04d22fb08
Daniel Schadd <danielschadd@Daniels-MacBook-Pro.local>
2020-12-14 13:56:11 -0600

    refactor for new buy online url

--- src/components/main/Rate.js
+++ src/components/main/Rate.js
@@ -92,11 +92,8 @@ function Rate({ t, match }) {
         // Build the Buy Online Button URL
         const baseUrl = process.env.REACT_APP_BUY_ONLINE_URL
         let quoteNumber = rate.id;
-        let zipCode     = quote.zip_code;
-        let carrier     = rate.carrier_id;
-        let product     = rate.carrier_product_id;
-        let language    = "en"
-        let buyOnline = `${baseUrl}?QuoteNumber=${quoteNumber}&ZipCode=${zipCode}&Carrier=${carrier}&Product=${product}&language=${language}`;
+        // Removing the "Q" in the quoteNumber
+        let buyOnline = `${baseUrl}/${quoteNumber.substring(1)}`;
 
         window.location.href = buyOnline
       }, 3000)
