59a238a18a7b1901f2d0ed013137195f9ae4ae19
Will Ramirez <wramirez09@users.noreply.github.com>
2020-12-21 11:36:36 -0600

    - comment back local server for local development purposes
    - comment back local server for local development purposes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import i18n from './i18n';
-import './styles/application.scss';
-import { createStore, applyMiddleware, compose } from 'redux'
-import thunk from 'redux-thunk';
-import siteReducer from './reducers';
-import Root from './Root';
-import * as serviceWorker from './serviceWorker';
-// import { makeServer } from "./server/server"
+import React from "react";
+import ReactDOM from "react-dom";
+import i18n from "./i18n";
+import "./styles/application.scss";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import siteReducer from "./reducers";
+import Root from "./Root";
+import * as serviceWorker from "./serviceWorker";
+import { makeServer } from "./server/server";
 
-// if (process.env.NODE_ENV === "development") { makeServer({ environment: "development" }) }
+if (process.env.NODE_ENV === "development") {
+  makeServer({ environment: "development" });
+}
 
-const composeEnhancers = (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true, traceLimit: 25 })) || compose;
+const composeEnhancers =
+  (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      trace: true,
+      traceLimit: 25,
+    })) ||
+  compose;
 
 const store = createStore(
   siteReducer,
@@ -21,22 +28,22 @@ const store = createStore(
 
 i18n
   .init({
-    fallbackLng: 'en',
+    fallbackLng: "en",
     debug: false,
-    ns: ['common', 'drivers', 'quotes', 'vehicles'],
+    ns: ["common", "drivers", "quotes", "vehicles"],
     returnObjects: true,
-    preload: ['en'],
+    preload: ["en"],
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
-    }
+    },
   })
   .then((t) => {
     ReactDOM.render(
       <React.StrictMode>
-        <Root store={store}/>
+        <Root store={store} />
       </React.StrictMode>,
-      document.getElementById('root')
-    )
+      document.getElementById("root")
+    );
   });
 
 // If you want your app to work offline and load faster, you can change
