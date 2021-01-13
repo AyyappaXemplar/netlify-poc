e336e6697587bc0c350e2e59d073b929d76b32d1
Juan Ortiz <juanfortizd@gmail.com>
2021-01-12 19:23:46 -0600

    refactor regex

--- src/components/App.js
+++ src/components/App.js
@@ -30,7 +30,7 @@ function App(props) {
     } else if (!quoteId) {
       setReady(true)
 
-      const allowedUrls = new RegExp(/(quotes\/new)|(quotes\/[-\w]*\/rates)|(not-covered)/)
+      const allowedUrls = new RegExp(/quotes\/(new|[-\w]*\/rates|not-covered)/)
       if (allowedUrls.test(window.location.pathname)) {
         return
       } else {
