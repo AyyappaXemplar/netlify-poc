60a6ab35414c496caff72f85975debbed25b8b10
Will Ramirez <wramirez@warriorinvictus.com>
2021-02-01 09:29:22 -0600

    add toggle to discounts section
    add toggle to discounts section

--- src/components/bind-online/driver/Discounts.js
+++ src/components/bind-online/driver/Discounts.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Col, FormLabel } from "react-bootstrap";
 import FormContainer from "../../shared/FormContainer";
 import Radio from "../../forms/Radio";
 import { withTranslation } from "react-i18next";
 
 const Discounts = ({ driver, updateParentState, t }) => {
+
+  const [discountsState, updateDiscountState] = useState({
+    defensive_driver: driver.defensive_driver,
+    good_driver: driver.good_driver,
+    good_student: driver.good_student,
+  });
+
+  const toggleValue = (value) => { 
+    if (value === true) {
+      return false
+    }
+    else { 
+      return true
+    }
+  }
+
   return (
     <Container>
       <FormContainer bootstrapProperties={{ md: 6 }}>
@@ -13,18 +29,25 @@ const Discounts = ({ driver, updateParentState, t }) => {
         <Row>
           <Col>
             {t("discounts").map((item, index) => {
-
-              return <Radio
-                type={item.type}
-                label={item.label}
-                value={item.value}
-                key={index}
-                selected={driver[item.label] === item.value}
-                name="radio_sr22"
-                onChange={() => {
-                  return updateParentState(true, item.label);
-                }}
-              />
+              return (
+                <Radio
+                  type={item.type}
+                  label={item.label}
+                  value={item.value}
+                  key={index}
+                  selected={discountsState[item.key]}
+                  name="radio_sr22"
+                  onChange={() => {
+       
+                    updateDiscountState((prevState) => { 
+                      const newDiscountState = { ...prevState }
+                      newDiscountState[item.key] = toggleValue(prevState[item.key])
+                      return newDiscountState
+                    })
+                    return updateParentState(!discountsState[item.key], item.key);
+                  }}
+                />
+              );
             })}
           </Col>
         </Row>
