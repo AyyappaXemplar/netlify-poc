84927360a358a3c48c07f1ede8b92158005c27ed
Will Ramirez <wramirez@warriorinvictus.com>
2021-02-02 14:50:00 -0600

    create policy details file
    create policy details file

---
+++ src/components/bind-online/quoteReview/PolicyCoverages.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FormContainer from "../../shared/FormContainer";
+import { Row, Col } from "react-bootstrap";
+export default function PolicyCoverages() {
+    return (
+        <FormContainer bootstrapProperties={{ md: 6 }} mbClass="mb-3">
+        <Row>
+         
+        </Row>
+      </FormContainer>
+    )
+}
