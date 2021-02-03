2952efc50d4118b6c65dd86ead397059d0a5e25f
Juan Ortiz <juanfortizd@gmail.com>
2021-02-03 10:40:21 -0600

    add effect for transitioning to next screen and add a Container to the screen

--- src/components/bind-online/driver/DriverForm.js
+++ src/components/bind-online/driver/DriverForm.js
@@ -1,36 +1,51 @@
 import React, { useState, useEffect }  from "react";
 import { useDispatch, useSelector }    from "react-redux"
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col, Form } from "react-bootstrap";
 
 import DriverDetails from "./DriverDetails";
 import LicenseInfo   from "./LicenseInfo"
 import Discounts     from "./Discounts"
+import SubmitButton  from "../../shared/SubmitButton"
 
+import history          from '../../../history';
 import { updateDriver } from "../../../actions/drivers"
 
 export default function DriverForm({ driver: driverProp, match }) {
-  const [driver, setDriver] = useState(false);
-  const dispatch  = useDispatch();
-  const drivers   = useSelector(state => state.data.quote.drivers)
+  const [driver, setDriver]         = useState(false)
+  const [submitting, setSubmitting] = useState(false)
+  const dispatch   = useDispatch();
+  const updatingStatus = useSelector(state => state.state.updatingDriver)
+  const drivers      = useSelector(state => state.data.quote.drivers)
 
-
+  // TODO: remove assigning driver from props when done with single page form
   useEffect(() => {
     let props
     if (match) {
-      props = drivers.find(driver => driver.id === match.paramsdriverId)
+      props = drivers.find(driver => driver.id === match.params.driverId)
     } else {
       props = driverProp
     }
-    const { marital_status='' } = props
-    setDriver({ ...props, marital_status })
+
+    const { first_name='', marital_status='' } = props
+    const violations = props.violations || []
+    setDriver({ ...props, first_name, marital_status, violations })
   }, [match, drivers, driverProp])
+
+  useEffect(() => {
+    if (!match) return
+
+    if (updatingStatus) {
+      setSubmitting(true)
+    } else if (submitting && !updatingStatus) {
+      history.push('/bol/quotes/drivers')
+    }
+  }, [updatingStatus, match, submitting])
+
 
   // TODO: we might not need to keep the state in sync with redux when we move to the URL workflow
   // useEffect(() => { updateDriverData(driver) }, [driver])
 
-
   const updateParentState = (value, key) => setDriver(prev => ({ ...prev, [key]: value }))
-  const dispatchDriver = () => dispatch(updateDriver(driver.id ,driver))
 
   const addViolation = (violation) => {
     setDriver((prevState) => {
@@ -39,18 +54,25 @@ export default function DriverForm({ driver: driverProp, match }) {
     });
   }
 
+  function handleSubmit(event) {
+    event.preventDefault()
+    dispatch(updateDriver(driver.id ,driver))
+  }
+
+  if (!driver) { return false }
+
   return (
-    <>
-      <DriverDetails driver={driver} updateParentState={updateParentState}/>
-      {/* <LicenseInfo driver={driver} updateParentState={updateParentState} addViolation={addViolation}/> */}
-      <Discounts driver={driver} updateParentState={updateParentState} />
-      <Container>
+    <Container>
+      <Form onSubmit={handleSubmit}>
+        <DriverDetails driver={driver} updateParentState={updateParentState}/>
+        <LicenseInfo driver={driver} updateParentState={updateParentState} addViolation={addViolation}/>
+        <Discounts driver={driver} updateParentState={updateParentState} />
         <Row>
-          <Col className="d-flex justify-content-center">
-            <Button className="rounded-pill my-3" variant="primary" onClick={dispatchDriver}>Save Driver</Button>
+          <Col md={{span: 6, offset: 3}} className="d-flex justify-content-center">
+            <SubmitButton text="Save Driver"/>
           </Col>
         </Row>
-      </Container>
-    </>
+      </Form>
+    </Container>
   );
 }
