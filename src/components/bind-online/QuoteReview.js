86d4b301b6716e78aaf0c3eeeece09c094942831
Will Ramirez <wramirez@warriorinvictus.com>
2021-02-08 09:21:30 -0600

    swapped out componenet

--- src/components/bind-online/QuoteReview.js
+++ src/components/bind-online/QuoteReview.js
@@ -1,43 +1,42 @@
-import React, { useState, useEffect }  from "react";
-import { useSelector, useDispatch }    from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Container, Row, Col, Button } from "react-bootstrap";
 
-import history       from "./../../history"
-import { rateQuote } from '../../actions/rates'
+import history from "./../../history";
+import { rateQuote } from "../../actions/rates";
 
 import DriverDetailsReview from "./quoteReview/DriverDetailsReview";
-import Drivers from "./quoteReview/Drivers";
 import PolicyCoverages from "./quoteReview/PolicyCoverages";
-import Vehicles from "./quoteReview/Vehicles"
-import Discounts from "./quoteReview/Discounts"
-
-
+import Vehicles from "./quoteReview/Vehicles";
+import Discounts from "./quoteReview/Discounts";
+import DriverReview from "./../bind-online/driver/DriverReview";
+import TitleRow from "./../shared/TitleRow";
 export const QuoteReview = () => {
-  const ratingQuote = useSelector(state => state.state.ratingQuote)
-  const dispatch    = useDispatch();
-  const [formSubmited, setFormSumbmitted] = useState(false)
+  const ratingQuote = useSelector((state) => state.state.ratingQuote);
+  const dispatch = useDispatch();
+  const [formSubmited, setFormSumbmitted] = useState(false);
+  const drivers = useSelector((redux) => {
+    return redux.data.quote.drivers;
+  });
 
   useEffect(() => {
-    if (formSubmited && !ratingQuote) history.push("/bol/rate")
-  }, [formSubmited, ratingQuote])
+    if (formSubmited && !ratingQuote) history.push("/bol/rate");
+  }, [formSubmited, ratingQuote]);
 
   const submitQuote = () => {
-    setFormSumbmitted(true)
-    dispatch(rateQuote(null, { type: 'final_quote' }))
-  }
+    setFormSumbmitted(true);
+    dispatch(rateQuote(null, { type: "final_quote" }));
+  };
 
   return (
     <>
       <Container>
-        <Row className="justify-content-center mb-5">
-          <Col md={6}>
-            <h1>Everything Looks Good?</h1>
-            <p>
-              Review what you’ve added so far. If everything looks good, you can
-              submit to get your policy.
-            </p>
-          </Col>
-        </Row>
+        <TitleRow
+          title={`Everything Looks Good?`}
+          subtitle={`Review what you’ve added so far. If everything looks good, you can
+              submit to get your policy.`}
+        />
+
         <Row className="justify-content-center">
           <Col md={6} className={"d-flex justify-content-between"}>
             <p>
@@ -49,20 +48,38 @@ export const QuoteReview = () => {
         </Row>
 
         <DriverDetailsReview />
+
         <PolicyCoverages />
         <Vehicles />
-        <Drivers />
-        <Discounts/>
-
-        <Row style={{display:"flex",justifyContent:"center", margin:'25px'}}>
+        <Row className={`justify-content-center`}>
           <Col xs={6}>
-            <Button onClick={submitQuote} className="rounded-pill" size='lg' block>
-              { ratingQuote ?  (
-                  <div className="spinner-border spinner-border-sm text-light" role="status">
-                    <span className="sr-only">Loading...</span>
-                  </div>
-                ) : 'Get a Quote'
-              }
+            {drivers.map((driver, index) => {
+              return <DriverReview driver={driver} />;
+            })}
+          </Col>
+        </Row>
+        <Discounts />
+
+        <Row
+          style={{ display: "flex", justifyContent: "center", margin: "25px" }}
+        >
+          <Col xs={6}>
+            <Button
+              onClick={submitQuote}
+              className="rounded-pill"
+              size="lg"
+              block
+            >
+              {ratingQuote ? (
+                <div
+                  className="spinner-border spinner-border-sm text-light"
+                  role="status"
+                >
+                  <span className="sr-only">Loading...</span>
+                </div>
+              ) : (
+                "Get a Quote"
+              )}
             </Button>
           </Col>
         </Row>
