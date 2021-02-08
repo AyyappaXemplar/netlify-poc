a364f62a1bf99295020c7321198caba525db9d8d
Will Ramirez <wramirez@warriorinvictus.com>
2021-02-08 10:37:20 -0600

    Update QuoteReview.js
    updated markup, and added in footer content

--- src/components/bind-online/QuoteReview.js
+++ src/components/bind-online/QuoteReview.js
@@ -11,6 +11,8 @@ import Vehicles from "./quoteReview/Vehicles";
 import Discounts from "./quoteReview/Discounts";
 import DriverReview from "./../bind-online/driver/DriverReview";
 import TitleRow from "./../shared/TitleRow";
+import BadgeText from "./../shared/BadgeText";
+
 export const QuoteReview = () => {
   const ratingQuote = useSelector((state) => state.state.ratingQuote);
   const dispatch = useDispatch();
@@ -38,19 +40,22 @@ export const QuoteReview = () => {
         />
 
         <Row className="justify-content-center">
-          <Col md={6} className={"d-flex justify-content-between"}>
+          <Col md={6} className="d-flex row justify-content-between">
             <p>
               <strong>Driver Details</strong>
             </p>
-
-            <strong>Edit</strong>
+            <button type="button" class="btn btn-link">
+              Edit
+            </button>
           </Col>
         </Row>
 
         <DriverDetailsReview />
 
         <PolicyCoverages />
+        
         <Vehicles />
+
         <Row className={`justify-content-center`}>
           <Col xs={6}>
             {drivers.map((driver, index) => {
@@ -59,10 +64,28 @@ export const QuoteReview = () => {
           </Col>
         </Row>
         <Discounts />
+        <Row className={`justify-content-center mb-5`}>
+          <Col
+            className={`d-flex flex-row-reverse justify-content-center align-items-center`}
+            xs={6}
+          >
+            <label for="disclaimer" className={`ml-2 mb-0`}>
+              I agree to the{" "}
+              <a href="http://www.google.com">following statements</a> to order
+              a Motor Vehicle Report.
+            </label>
+            <input type="checkbox" id="disclaimer" name="disclainer" />
+          </Col>
+        </Row>
 
-        <Row
-          style={{ display: "flex", justifyContent: "center", margin: "25px" }}
-        >
+        <Row className="justify-content-center mb-5">
+          <Col xs={6}>
+            <strong>Note:</strong> If you cannot agree with the following
+            statements please contact our agents in regards to any questions.
+            When calling, please have your quote number ready.
+          </Col>
+        </Row>
+        <Row className={`justify-content-center mb-2`}>
           <Col xs={6}>
             <Button
               onClick={submitQuote}
@@ -81,6 +104,42 @@ export const QuoteReview = () => {
                 "Get a Quote"
               )}
             </Button>
+          </Col>
+        </Row>
+        <Row className="mb-5 justify-content-center">
+          <Col xs={6} className="justify-content-center d-flex row">
+            <button type="button" class="btn btn-link">
+              Cancel & Return
+            </button>
+          </Col>
+        </Row>
+
+        <BadgeText />
+
+        <Row className="justify-content-center mb-5">
+          <Col
+            className="justify-content-center"
+            xs={6}
+            style={{ textAlign: "center" }}
+          >
+            <p>
+              <strong>Contact us</strong>
+            </p>
+            <button type="button" class="btn btn-link" href="tel:8583585605">
+              (844) 358-5605
+            </button>
+            <br />
+            <button
+              type="button"
+              class="btn btn-link"
+              href="mailto:agent@insureonline.com"
+            >
+              agent@insureonline.com
+            </button>
+            <p>
+              6640 S Cicero Ave
+              <br /> Bedford Park, IL 60638
+            </p>
           </Col>
         </Row>
       </Container>
