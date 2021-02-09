1db5d639bb7c0644a8783745a447f7e7e941db48
Juan Ortiz <juanfortizd@gmail.com>
2021-02-08 18:00:38 -0600

    replace form container

--- src/components/bind-online/quoteReview/PolicyTerm.js
+++ src/components/bind-online/quoteReview/PolicyTerm.js
@@ -7,23 +7,23 @@ import getDate from "../../../services/timestamps";
 export default function DriverDetailsReview({quote}) {
 
   return (
-    <FormContainer bootstrapProperties={{ md: 6 }}>
-      <Container>
-        <Row>
-          <Col xs={12} md={6}>
+    <Row>
+      <Col lg={{span: 6, offset: 3}}>
+        <Row className='bg-white rounded shadow-sm mb-5 p-4'>
+          <Col lg={6}>
             <p>
               <strong>Policy Term</strong>
             </p>
-            <p>{quote.term.duration} months</p>
+            <p className="mb-0">{quote.term.duration} months</p>
           </Col>
           <Col xs={12} md={6}>
             <p>
               <strong>Effective Dates</strong>
             </p>
-            <p>{getDate(quote.term.effective, 'MM/DD/YYYY')} - {getDate(quote.term.expires, 'MM/DD/YYYY')}</p>
+            <p className="mb-0">{getDate(quote.term.effective, 'MM/DD/YYYY')} - {getDate(quote.term.expires, 'MM/DD/YYYY')}</p>
           </Col>
         </Row>
-      </Container>
-    </FormContainer>
+      </Col>
+    </Row>
   );
 }
