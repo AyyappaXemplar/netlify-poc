72e222d756e1a77e52b7ce19c04f7e3ef6e2d672
Will Ramirez <wramirez@warriorinvictus.com>
2021-02-09 13:57:35 -0600

    update, route, questions, and added button and badge text
    update, route, questions, and added button and badge text

--- src/components/bind-online/Questions.js
+++ src/components/bind-online/Questions.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
-import { Row, Col, Form } from "react-bootstrap";
+import { Row, Col, Form, Button } from "react-bootstrap";
 import FormContainer from "../shared/FormContainer";
 import TitleRow from "../shared/TitleRow";
+import BadgeText from "../shared/BadgeText";
 
 const Questions = () => {
   const questions = useSelector((redux) => redux.data.quote.questions);
-  console.log(questions);
+  // const [questionsState, updateQuestionsState] = useState(questions);
+
+  const handleCheckOnChange = (question) => {};
+
   return (
     <>
       <TitleRow
@@ -19,28 +23,59 @@ const Questions = () => {
             return (
               <>
                 <Row className="justify-content-center align-items-center mb-3 boder-bottom-dark">
-                  <Col xs={12} md={8} className="pr-5">
+                  <Col xs={12} md={9} className="pr-5">
                     <label>{question.text}</label>
                   </Col>
-                  <Col xs={6} md={4} className="d-flex row justify-content-between align-items-center">
+                  <Col
+                    xs={6}
+                    md={3}
+                    className="d-flex row justify-content-around align-items-center"
+                  >
                     <div>
-                      <input type="radio" name={`question-${question.question_code}`} />
-                      <label className="mb-0 ml-2">Yes</label>
+                      <input
+                        type="radio"
+                        name={`question-${question.question_code}`}
+                        onChange={(e) => {
+                          handleCheckOnChange(question, e);
+                        }}
+                      />
+                      <label className="mb-0 ml-2" value={true}>
+                        Yes
+                      </label>
                     </div>
                     <div>
-                      <input type="radio" name={`question-${question.question_code}`} />
-                      <label className="mb-0 ml-2">No</label>
+                      <input
+                        type="radio"
+                        name={`question-${question.question_code}`}
+                        onChange={(e) => {
+                          handleCheckOnChange(question, e);
+                        }}
+                      />
+                      <label className="mb-0 ml-2" value={false}>
+                        No
+                      </label>
                     </div>
                   </Col>
                 </Row>
-                { 
-                    index >= questions.length -1 ? null : <hr />
-                }
+                {index >= questions.length - 1 ? null : <hr />}
               </>
             );
           })}
         </Form>
       </FormContainer>
+      <Row className="justify-content-center">
+        <Col xs={6} className="d-flex row justify-content-center">
+          <Button className="rounded-pill col-8 mb-5">Save & Continue</Button>
+        </Col>
+      </Row>
+      <Row className="justify-content-center mb-5">
+        <Col xs={6} className="d-flex row justify-content-center">
+          <button type="button" class="btn btn-link">
+            Cancel & Return
+          </button>
+        </Col>
+          </Row>
+          <BadgeText />
     </>
   );
 };
