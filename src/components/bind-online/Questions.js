22693bd8a096f6bc70dc7451d58e85548e199ad6
Will Ramirez <wramirez@warriorinvictus.com>
2021-02-09 13:21:50 -0600

    clean up and remove hr from last item

--- src/components/bind-online/Questions.js
+++ src/components/bind-online/Questions.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Container, Row, Col, Button, Form } from "react-bootstrap";
+import { Row, Col, Form } from "react-bootstrap";
 import FormContainer from "../shared/FormContainer";
 import TitleRow from "../shared/TitleRow";
 
@@ -15,21 +15,6 @@ const Questions = () => {
       />
       <FormContainer bootstrapProperties={{ md: 6 }}>
         <Form>
-          {/* <Row className="justify-content-center align-items-center mb-3 boder-bottom-dark">
-            <Col xs={8}>
-              <label>lorem</label>
-            </Col>
-            <Col xs={4} className="d-flex row justify-content-between align-items-center">
-              <div>
-                <input type="radio" name="question"/>
-                <label className="mb-0 ml-2">Yes</label>
-              </div>
-              <div>
-                <input type="radio" name="question"/>
-                <label className="mb-0 ml-2">No</label>
-              </div>
-            </Col>
-          </Row> */}
           {questions.map((question, index) => {
             return (
               <>
@@ -37,28 +22,20 @@ const Questions = () => {
                   <Col xs={12} md={8} className="pr-5">
                     <label>{question.text}</label>
                   </Col>
-                  <Col
-                    xs={6}
-                    md={4}
-                    className="d-flex row justify-content-between align-items-center"
-                  >
+                  <Col xs={6} md={4} className="d-flex row justify-content-between align-items-center">
                     <div>
-                      <input
-                        type="radio"
-                        name={`question-${question.question_code}`}
-                      />
+                      <input type="radio" name={`question-${question.question_code}`} />
                       <label className="mb-0 ml-2">Yes</label>
                     </div>
                     <div>
-                      <input
-                        type="radio"
-                        name={`question-${question.question_code}`}
-                      />
+                      <input type="radio" name={`question-${question.question_code}`} />
                       <label className="mb-0 ml-2">No</label>
                     </div>
                   </Col>
                 </Row>
-                <hr />
+                { 
+                    index >= questions.length -1 ? null : <hr />
+                }
               </>
             );
           })}
