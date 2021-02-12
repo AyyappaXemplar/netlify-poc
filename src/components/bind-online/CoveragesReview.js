159e2867c23a4127e13c159cac34f996f732b3c4
Juan Ortiz <juanfortizd@gmail.com>
2021-02-12 16:22:26 -0600

    remove logic since it will not be used anymore

--- src/components/bind-online/CoveragesReview.js
+++ src/components/bind-online/CoveragesReview.js
@@ -3,25 +3,21 @@ import { useSelector }                  from 'react-redux';
 import { withTranslation }              from 'react-i18next';
 import { Container, Col, Row, Image }   from 'react-bootstrap';
 
-import {Link}                             from 'react-router-dom';
+import { Link }                         from 'react-router-dom';
 import TitleRow                         from '../shared/TitleRow';
 import icon                             from "../../images/stacked_icon_lg.svg";
 import PolicyCoverages                  from "./quoteReview/PolicyCoverages";
-import Vehicles                         from "./quoteReview/Vehicles";
+import Vehicle                          from "../rate/Vehicle";
 import BadgeText                        from "../shared/BadgeText";
 import FooterContent                    from "../shared/FooterContent"
 
-
-
 function Coverages({ t, match }) {
-
-  const allCars = useSelector((state) => state.data.quote.vehicles )
   const quote = useSelector(state => state.data.quote)
 
   return (
     <Container>
       <TitleRow title={"Review your coverage."} subtitle={"You can review your Basic Coverage option below. "} />
-    
+
       <Row className="d-flex flex-column justify-content-center align-items-center">
       <Col lg={6} className={"d-flex row"}>
           <div className="bg-white p-4 w-100 shadow rounded d-flex">
@@ -39,11 +35,15 @@ function Coverages({ t, match }) {
 
       <Row className="justify-content-center">
         <Col sm={6} className="justify-content-center">
-          <Vehicles vehicles={allCars} displayCoverageSelector={false} forceShowEditUi={true} hideEyebrowText={true}/>
+          <div className='mb-5'>
+            {quote.vehicles.map(vehicle =>
+              <Vehicle vehicle={vehicle} displayCoverageSelector={false} forceShowEditUi={true}/>
+            )}
+          </div>
         </Col>
       </Row>
 
-          
+
       <Row className="justify-content-center mb-5">
         <Col sm={5} className="d-flex justify-content-center mb-5 flex-column">
         <Link className="rounded-pill btn btn-primary btn-block btn-lg mb-3" to={'/bol/questions'}>Save and Continue</Link>
@@ -53,7 +53,7 @@ function Coverages({ t, match }) {
       </Row>
 
       <FooterContent/>
-      
+
     </Container>
   )
 }
