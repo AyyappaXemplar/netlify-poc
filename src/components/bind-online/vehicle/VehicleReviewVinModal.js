d454a546b46a65b3f856a58211bff0ec584ad5ed
Will Ramirez <wramirez@warriorinvictus.com>
2021-02-16 15:25:24 -0600

    scaffold out UI for vehicles vin number modal
    scaffold out UI for vehicles vin number modal

---
+++ src/components/bind-online/vehicle/VehicleReviewVinModal.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Modal, Button, Image } from "react-bootstrap";
+import { withTranslation } from "react-i18next";
+
+import CircleIcon from "../../shared/CircleIcon";
+import iconImage from "../../../images/sheild_icon.svg";
+import vinGraph from "../../../images/vin_graph.png"
+
+const VehicleRevieVinModal = ({ showVinModalState, updateShowVinModalState, t }) => {
+  return (
+    <>
+      <Modal
+        show={showVinModalState}
+        onHide={()=>updateShowVinModalState(false)}
+        size={"md"}
+        className={"reviewPageModal"}
+      >
+        <Modal.Header closeButton className="border-0"></Modal.Header>
+        <Modal.Body className="flex-column px-5 pb-5 pt-0">{/**override bootstrap text align - rule  */}
+          <CircleIcon iconSrc={iconImage} />
+          <p className="p-3 mb-3">
+            <strong>{t("VehiclesVinModal.header")}</strong>
+          </p>
+          <p>{ t("VehiclesVinModal.copy")}</p>
+          <Image src={vinGraph} className="p-3" fluid/>
+          
+          <Button
+            className="rounded-pill btn btn-primary btn-block btn-lg"
+            size="lg"
+            onClick={()=> updateShowVinModalState(false)}
+          >Close
+          </Button>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+};
+
+export default withTranslation(["modals"])(VehicleRevieVinModal);
