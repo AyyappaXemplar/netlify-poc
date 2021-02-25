4c81529de6867fdebc0102b66c90ef289d99a030
will ramirez <wramirez1980@gmail.com>
2021-02-25 10:49:39 -0600

    bug fix for display error
    bug fix for display error

--- src/components/bind-online/quoteReview/PolicyCoverages.js
+++ src/components/bind-online/quoteReview/PolicyCoverages.js
@@ -1,13 +1,16 @@
-import React     from "react";
-
-import IconListItem from "../../shared/bind-online/IconListItem";
-import StackedIcon  from '../../shared/stacked_icon_lg';
+import React                from "react";
+import { withTranslation }  from 'react-i18next';
+import IconListItem         from "../../shared/bind-online/IconListItem";
+import StackedIcon          from '../../shared/stacked_icon_lg';
 
 import { getPolicyCoveragesFromQuote, getCoverageValues } from '../../../services/coverages'
 
-export default function PolicyCoverages({ quote, children, strength, showBottomText=true, coverageData }) {
+function PolicyCoverages({ quote, children, strength, showBottomText = true, t }) {
+  
+  const coverages = getPolicyCoveragesFromQuote(quote);
+  
+  const content = t(`coverages.${strength}`)
 
-  const coverages = getPolicyCoveragesFromQuote(quote)
 
   const renderPolicies = () => {
     return coverages.map((coverage, index) => {
@@ -31,11 +34,11 @@ export default function PolicyCoverages({ quote, children, strength, showBottomT
 
         <div className="py-4">
           <div className="d-flex flex-row align-items-center coverage-note">
-            <div className='mr-3 svg-container'>
+            <div className='mr-3 svg-container col-2'>
               <StackedIcon strength={strength}/>
             </div>
             <div>
-              {coverageData.message}
+             <small> {content.message}</small>
             </div>
           </div>
         </div>
@@ -43,3 +46,6 @@ export default function PolicyCoverages({ quote, children, strength, showBottomT
     </div>
   );
 }
+
+
+export default withTranslation(['vehicles'])(PolicyCoverages)
