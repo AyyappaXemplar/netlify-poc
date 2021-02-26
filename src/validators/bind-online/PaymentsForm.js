421a2a5cf67a9fae890f094952059277758711aa
will ramirez <wramirez1980@gmail.com>
2021-02-26 16:47:26 -0600

    warp validation functions in a conditional
    warp validation functions in a conditional

--- src/validators/bind-online/PaymentsForm.js
+++ src/validators/bind-online/PaymentsForm.js
@@ -1,39 +1,54 @@
 import payment from 'payment';
 const validate = require("validate.js");
 
-validate.validators.ccNumberValidator = (included, options, key, attributes) => { 
-  var valid = payment.fns.validateCardNumber(attributes.credit_card.number);
-  if (!valid) {
-    return "-cc number not valid"
+validate.validators.ccNumberValidator = (included, options, key,attributes) => {
+  if (attributes.credit_card) {
+    var valid = payment.fns.validateCardNumber(attributes.credit_card.number);
+    if (!valid) {
+      return "-cc number not valid";
+    } else {
+      return null;
+    }
   }
   else { 
     return null
   }
-}
+};
 
 
 validate.validators.ccExpirationValidator = (included, options, key, attributes) => {
+  if (attributes.credit_card) {
+    const isExpirationValid = payment.fns.validateCardExpiry(attributes.credit_card.exp_month, attributes.credit_card.exp_year)
 
-  const isExpirationValid = payment.fns.validateCardExpiry(attributes.credit_card.exp_month, attributes.credit_card.exp_year)
-
-  if (!isExpirationValid) {
-    return "- expiration date not valid"
+    if (!isExpirationValid) {
+      return "- expiration date not valid"
+    }
+    else {
+      return null
+    }
   }
   else { 
     return null
   }
+    
 }
 
 validate.validators.cvcValidator = (included, options, key, attributes) => {
+  if (attributes.credit_card) {
+    const isCVvvValid = payment.fns.validateCardCVC(attributes.credit_card.cvv)
 
-  const isCVvvValid = payment.fns.validateCardCVC(attributes.credit_card.cvv)
-
-  if (!isCVvvValid) {
-    return 'CVC code is invalid'
+    if (!isCVvvValid) {
+      return 'CVC code is invalid'
+    }
+    else {
+      return null
+    }
   }
   else { 
     return null
   }
+
+  
   
 }
 
@@ -50,7 +65,18 @@ const paymentsValidator = {
   },
   cc_cvv: {
     cvcValidator: true
-  }
+  },
+
+  // 'bankAccountValidation': function (value, attributes, attributeName, options, constraints) {
+  //   // if (options.paymentMethod === 'credit_card') {
+  //   //   const pattern = /\d{3,4}/
+  //   //   return {
+  //   //     format: { pattern },
+  //   //   }
+  //   // } else {
+  //   //   return false
+  //   // }
+  // }
 }
 
 export default function validatePolicyDetails(paymentParams, options) {
