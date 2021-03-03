e60050ccaf79a4e7ae9ae34916dbdc56a9a307e8
will ramirez <wramirez1980@gmail.com>
2021-03-03 11:49:51 -0600

    remove extra bangs
    remove extra bangs

--- src/components/bind-online/QuoteDrivers.js
+++ src/components/bind-online/QuoteDrivers.js
@@ -15,9 +15,8 @@ const QuoteDrivers = () => {
   const quote = useSelector(state => state.data.quote)
   const vehicleId = quote.vehicles[0].id
   const validatedDrivers = quote.drivers.map((driverObj) => { 
-    return {...driverObj, isValid: !!!validateDriver(driverObj)}
+    return {...driverObj, isValid: !validateDriver(driverObj)}
   })
-
 
   return (
     <Container>
