1e997f1ff8d28dfd6ed936b4f97915f9634564cb
will ramirez <wramirez1980@gmail.com>
2021-03-05 13:29:59 -0600

    clean up payment details section
    clean up payment details section

--- src/components/bind-online/Confirmation/PaymentDetails.js
+++ src/components/bind-online/Confirmation/PaymentDetails.js
@@ -5,10 +5,10 @@ const PaymentDetails = () => {
     <Row className="justify-content-center mb-3">
       <Col className="justify-content-center bg-white rounded shadow p-5" lg={6}>
         <p className="mb-3"><strong>Payment Details</strong></p>
-          <Row className="mb-3">
-            <Col><p>Amount Charged: $105.42</p><p>MasterCard **** 7712</p></Col>
-            <Col><p>MasterCard **** 7712</p><p>Transaction ID: 123FA67Z</p></Col>
-        </Row>
+          {/* <Row className="mb-3">
+            <Col><p>&nbsp;</p><p>&nbsp;</p></Col>
+            <Col><p>&nbsp;</p><p>&nbsp;</p></Col>
+        </Row> */}
         <Row>
           <Col className="p-3 pink-bg">
             <p className="m-0"><strong>If paying with installments</strong> - All additional payments will be billed by the insurance carrier.</p>
