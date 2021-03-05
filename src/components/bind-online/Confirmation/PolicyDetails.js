c4c7ae18b74e282a24a0a11402b5f9b015c6dc40
will ramirez <wramirez1980@gmail.com>
2021-03-05 15:34:51 -0600

    remove some hard coded data and add phone number to carrier component
    remove some hard coded data and add phone number to carrier component

--- src/components/bind-online/Confirmation/PolicyDetails.js
+++ src/components/bind-online/Confirmation/PolicyDetails.js
@@ -6,20 +6,20 @@ import CarrierComponent     from '../../../components/rate/Carrier';
 import getDate              from '../../../services/timestamps'
 
 
-const PolicyDetails = ({quote}) => {
-
-  const { carrier } = useSelector(redux => redux.data);
+const PolicyDetails = ({ carrier }) => {
+  
   const { documents, term } =  useSelector(redux => redux.data)
  
   return (
     <Row className="justify-content-center"> 
       <Col lg={6} className={"bg-white rounded shadow py-3 px-5"}>
         <Row>
-          <CarrierComponent carrier={carrier[0]} hasBorder={false}/>
-          <Col className="border-top border-bottom py-3">
+          <CarrierComponent carrier={carrier} hasBorder={false} />
+          
+          {/* <Col className="border-top border-bottom py-3">
             <p><strong>Policy Number</strong></p>
             <p className="mb-0">RQS15894B3G</p>
-          </Col>
+          </Col> */}
           <Col className="border-top border-bottom py-3">
             <p><strong>Effective Date</strong></p>
             <p className="mb-0">{getDate(term.effective)} to {getDate(term.expiration)}</p>
@@ -27,6 +27,7 @@ const PolicyDetails = ({quote}) => {
         </Row>
         <Row>
           <Col>
+          <p className="mt-3"><strong>Coverage Documents</strong></p>
             {documents.map((link, i) => { 
               return <div key={i+1 } className="py-3 d-flex justify-content-between"> <p>{link.type}</p><p key={i+1}><Image src={dlIcon} />&nbsp;<a href={link.url} className="orange">DownLoad</a></p></div>
             })}
