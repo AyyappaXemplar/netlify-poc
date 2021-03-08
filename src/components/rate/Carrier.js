52271897f639d6d2cf2321d8b7041dd0ddc5a222
Juan Ortiz <juanfortizd@gmail.com>
2021-03-08 14:35:00 -0600

    move border outside component

--- src/components/rate/Carrier.js
+++ src/components/rate/Carrier.js
@@ -1,10 +1,9 @@
 import React      from 'react'
 import ReactStars from "react-rating-stars-component";
 import { useSelector } from 'react-redux'
-export default function Carrier({ carrier, hasBorder=true, documents, term }) {
 
-  const addBrderClass = hasBorder ? "border" : "";
   const {updatingQuoteInfo, fetchingQuoteDocumets} = useSelector(redux => redux.state.updatingQuoteInfo);
+export default function Carrier({ carrier, documents, term }) {
   const formatPhoneNumber = (phoneNumberString) => {
     var cleaned = ('' + phoneNumberString).replace(/\D/g, '')
     var match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/)
@@ -18,7 +17,7 @@ export default function Carrier({ carrier, hasBorder=true, documents, term }) {
     return <>Customer Service: &nbsp;<a href={`tel:${carrier.phone}` } className="text-dark"><u className="orange">{formatPhoneNumber(carrier.phone)}</u></a></>
   }
   return (
-    <div className={`${addBrderClass} p-4`}>
+    <>
       <div className="d-flex mb-3 flex-column flex-md-row">
         <div style={{maxWidth: '200px'}}>
           <img style={{width: '100%'}} src={`https://wi-sirius-production.nyc3.cdn.digitaloceanspaces.com/assets/carriers/logos/${carrier.tag.toLowerCase()}.png`} alt="carrier"/>
