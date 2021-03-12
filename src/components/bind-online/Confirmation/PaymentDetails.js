6ab93ffa644ca094f954a3a86fe1b4e5195e6a5e
will ramirez <wramirez1980@gmail.com>
2021-03-12 13:05:22 -0600

    leverage getDeposit function

--- src/components/bind-online/Confirmation/PaymentDetails.js
+++ src/components/bind-online/Confirmation/PaymentDetails.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Row, Col } from 'react-bootstrap'
+import { getDeposit } from '../../../services/rate-payment-details'
 const PaymentDetails = ({deposit}) => {
   return (
     <Row className="justify-content-center mb-3">
       <Col className="justify-content-center bg-white rounded shadow p-5" lg={6}>
         <p className="mb-3"><strong>Payment Details</strong></p>
           <Row className="mb-3">
-          <Col><p>Amount Charged: ${deposit}</p><p></p></Col>
+          <Col><p>Amount Charged: { getDeposit(deposit)}</p><p></p></Col>
             <Col><p>&nbsp;</p><p>&nbsp;</p></Col>
         </Row>
         <Row>
