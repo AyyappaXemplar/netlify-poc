8949bee56904acc7e663fcdebd48cd9e93987b95
Juan Ortiz <juanfortizd@gmail.com>
2021-03-12 10:37:56 -0600

    add progress bars for bol and final process

--- src/constants/progress-bar.js
+++ src/constants/progress-bar.js
@@ -1,15 +1,45 @@
 export const progressBarPercentages = {
   START:     8,
-  VEHICLES:  32,
-  DRIVERS:   62,
-  DISCOUNTS: 95,
+  VEHICLES:  33,
+  DRIVERS:   65,
+  // DISCOUNTS: 95,
   REVIEW:    100
 }
 
-export const ProgressBarStatus = {
+export const progressBarStatus = {
   START:     'START',
   DRIVERS:   'DRIVERS',
   VEHICLES:  'VEHICLES',
   DISCOUNTS: 'DISCOUNTS',
   REVIEW:    'REVIEW'
+}
+
+export const bolProgressBarPercentages = {
+  POLICY:     5,
+  DRIVERS:   20,
+  VEHICLES:  38,
+  COVERAGES: 58,
+  QUESTIONS: 78,
+  REVIEW:    100
+}
+
+export const bolProgressBarStatus = {
+  POLICY:    'POLICY',
+  DRIVERS:   'DRIVERS',
+  VEHICLES:  'VEHICLES',
+  COVERAGES: 'COVERAGES',
+  QUESTIONS: 'QUESTIONS',
+  REVIEW:    'REVIEW'
+}
+
+export const finalProgressBarPercentages = {
+  PAYMENTS:      9,
+  SIGNATURES:   49,
+  CONFIRMATION: 100
+}
+
+export const finalProgressBarStatus = {
+  PAYMENTS:     'PAYMENTS',
+  SIGNATURES:   'SIGNATURES',
+  CONFIRMATION: 'CONFIRMATION'
 }
