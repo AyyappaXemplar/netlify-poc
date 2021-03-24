8e6a55016df356a4e11babdb770b5b310e5d8b3f
will ramirez <wramirez1980@gmail.com>
2021-03-24 13:55:59 -0500

    add copy to review screen, center text and icons on bol rates page
    add copy to review screen, center text and icons on bol rates page

--- src/components/shared/CoverageStrength.js
+++ src/components/shared/CoverageStrength.js
@@ -17,7 +17,7 @@ function CoverageStrength({strength}) {
   const barFills = BAR_FILLS[strength]
 
   return (
-    <div className="coverage-graph-item">
+    <div className="coverage-graph-item d-flex align-items-center">
       <svg width="1rem" height="1rem" viewBox="0 0 16 16"
            fill="currentColor"
            xmlns="http://www.w3.org/2000/svg"
