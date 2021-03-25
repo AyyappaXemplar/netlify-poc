763396ed4c35089da4935603c379909d337cd59b
will ramirez <wramirez1980@gmail.com>
2021-03-25 16:44:47 -0500

    fix payments layout

--- src/components/bind-online/payments/CardForm.js
+++ src/components/bind-online/payments/CardForm.js
@@ -33,7 +33,7 @@ const CardForm = ({ creditCard, setCreditCard }) => {
   return (
     <div className="mb-4">
      <Row>
-       <Col lg={6}>
+       <Col lg={6} className="pr-0">
          <Form.Group>
            <Form.Label>First Name</Form.Label>
            <Form.Control type="name" name="first_name" value={creditCard.name}
@@ -49,35 +49,28 @@ const CardForm = ({ creditCard, setCreditCard }) => {
        </Col>
      </Row>
       <Row>
-        <Col lg={6}>
-          <Row>
-          <Form.Group className="col-6 pr-0">
-            <Form.Label>Card Number</Form.Label>
-            <Form.Control type="text" placeholder="1111 1111 1111 111"
-                name="number" value={creditCard.number} onChange={changeCreditCard} />
-               <Image src={icon_cc} className="mt-2"/>
-          </Form.Group>
-            <Form.Group className="col-3 pr-0">
-            <Form.Label>Exp. Month</Form.Label>
-            <Form.Control type="text" placeholder="MM"
-              name="exp_month" value={creditCard.exp_month} onChange={changeCreditCard} maxLength="2"/>
-            </Form.Group>
-             
-            <Form.Group className="col-3 pr-0">
-            <Form.Label>Exp. Year</Form.Label>
-            <Form.Control type="text" placeholder="YY"
-              name="exp_year" value={creditCard.exp_year} onChange={changeCreditCard} maxLength="2"/>
-            </Form.Group>
-           
-          </Row>
-        </Col>
-        <Col className="col-4 mb-12">
-        <Form.Group>
-            <Form.Label>Security Code</Form.Label>
-            <Form.Control type="text" placeholder="CVV" maxLength="4" minLength="3"
-              name="cvv" value={creditCard.cvv} onChange={changeCreditCard} />
-            </Form.Group>
-        </Col>
+      <Form.Group className="col-6 pr-0">
+        <Form.Label>Card Number</Form.Label>
+        <Form.Control type="text" placeholder="1111 1111 1111 111"
+            name="number" value={creditCard.number} onChange={changeCreditCard} />
+            <Image src={icon_cc} className="mt-2"/>
+      </Form.Group>
+        <Form.Group className="col-3 pr-0">
+        <Form.Label>Exp. Month</Form.Label>
+        <Form.Control type="text" placeholder="MM"
+          name="exp_month" value={creditCard.exp_month} onChange={changeCreditCard} maxLength="2"/>
+        </Form.Group>
+          
+        <Form.Group className="col-3 pr-0">
+        <Form.Label>Exp. Year</Form.Label>
+        <Form.Control type="text" placeholder="YY"
+          name="exp_year" value={creditCard.exp_year} onChange={changeCreditCard} maxLength="2"/>
+        </Form.Group>
+        <Form.Group className="col-4 mb-12">
+        <Form.Label>Security Code</Form.Label>
+        <Form.Control type="text" placeholder="CVV" maxLength="4" minLength="3"
+          name="cvv" value={creditCard.cvv} onChange={changeCreditCard} />
+        </Form.Group>
       </Row>
     </div>
   );
