e2181d3c2be5c10fa937c3b0a5ffcc1ebee043c3
will ramirez <wramirez1980@gmail.com>
2021-03-30 10:02:51 -0500

    update finals page layout

--- src/components/bind-online/Confirmation/PolicyDetails.js
+++ src/components/bind-online/Confirmation/PolicyDetails.js
@@ -13,18 +13,7 @@ const PolicyDetails = ({ carrier, document, term, policy_number, deposit }) => {
       <Col lg={6} className={"bg-white rounded shadow py-3 px-5"}>
         <Row>
           <CarrierComponent carrier={carrier} hasBorder={false}/>
-          <Col className="justify-content-center">
-            <p className="mb-3"><strong>Payment Details</strong></p>
-              <Row className="mb-3">
-                <Col><p>Amount Charged: ${getDeposit({deposit})}</p><p></p></Col>
-                <Col><p>&nbsp;</p><p>&nbsp;</p></Col>
-            </Row>
-            <Row>
-              <Col className="p-3 pink-bg">
-                <p className="m-0"><strong>If paying with installments</strong> - All additional payments will be billed by the insurance carrier.</p>
-              </Col>
-            </Row>
-          </Col>
+        
           <Col className="border-top border-bottom py-3">
             <p><strong>Policy Number</strong></p>
             <p className="mb-0">{policy_number}</p>
@@ -46,6 +35,22 @@ const PolicyDetails = ({ carrier, document, term, policy_number, deposit }) => {
             </div>
           </Col>
         </Row>
+
+
+        <Row className="justify-content-center">
+            <Col xs={12}><p className="mb-3"><strong>Payment Details</strong></p></Col>
+            <Col className="mb-3">
+                <div><p>Amount Charged: ${getDeposit({deposit})}</p><p></p></div>
+            </Col>
+            <Row>
+              <Col className="p-3 pink-bg" >
+                <p className="m-0"><strong>If paying with installments</strong> - All additional payments will be billed by the insurance carrier.</p>
+              </Col>
+            </Row>
+        </Row>
+
+
+
       </Col>
     </Row>
   )
