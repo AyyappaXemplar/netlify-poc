4f6f4d8116a794301d66ca46fba08d9da9f6b652
johnathan guzman <jguzman@priscorp.net>
2021-04-01 15:41:06 -0500

    Added padding to some comp. Changed layout on LG breakpoint, from 3 per row to 2. WIP.

--- src/components/bind-online/payments/CardForm.js
+++ src/components/bind-online/payments/CardForm.js
@@ -33,14 +33,14 @@ const CardForm = ({ creditCard, setCreditCard }) => {
   return (
     <div className="mb-4">
      <Row>
-        <Col lg={6} className="pr-lg-2">
+        <Col lg={6} className="pr-lg-1">
          <Form.Group>
            <Form.Label>First Name</Form.Label>
            <Form.Control type="name" name="first_name" value={creditCard.name}
               placeholder="John" onChange={changeCreditCard} />
         </Form.Group>
         </Col>
-        <Col lg={6} className="pl-lg-2">
+        <Col lg={6} className="pl-lg-1">
           <Form.Group>
             <Form.Label>Last Name</Form.Label>
            <Form.Control type="name" name="last_name" value={creditCard.name}
@@ -49,19 +49,19 @@ const CardForm = ({ creditCard, setCreditCard }) => {
        </Col>
      </Row>
       <Row>
-      <Form.Group className="col-md-6 pr-md-2">
+      <Form.Group className="col-md-6 pr-md-1">
         <Form.Label>Card Number</Form.Label>
         <Form.Control type="text" placeholder="1111 1111 1111 111"
             name="number" value={creditCard.number} onChange={changeCreditCard} />
             <Image src={icon_cc} className="mt-2"/>
       </Form.Group>
-        <Form.Group className="col-md-3 col-lg-6 col-xl-3 px-md-2">
+        <Form.Group className="col-md-3 col-lg-6 col-xl-3 px-md-1">
         <Form.Label>Exp. Month</Form.Label>
         <Form.Control type="text" placeholder="MM"
           name="exp_month" value={creditCard.exp_month} onChange={changeCreditCard} maxLength="2"/>
         </Form.Group>
           
-        <Form.Group className="col-md-3 col-lg-6 col-xl-3 pl-md-2">
+        <Form.Group className="col-md-3 col-lg-6 col-xl-3 pl-md-1">
         <Form.Label>Exp. Year</Form.Label>
         <Form.Control type="text" placeholder="YY"
           name="exp_year" value={creditCard.exp_year} onChange={changeCreditCard} maxLength="2"/>
