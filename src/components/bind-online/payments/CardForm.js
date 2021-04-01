1349ca4dcf267edc45033e1f2a9f545c095fab06
johnathan guzman <jguzman@priscorp.net>
2021-04-01 15:15:10 -0500

    Added breakpoints to avoid exp date break from layout

--- src/components/bind-online/payments/CardForm.js
+++ src/components/bind-online/payments/CardForm.js
@@ -55,18 +55,18 @@ const CardForm = ({ creditCard, setCreditCard }) => {
             name="number" value={creditCard.number} onChange={changeCreditCard} />
             <Image src={icon_cc} className="mt-2"/>
       </Form.Group>
-        <Form.Group className="col-md-3 px-md-2">
+        <Form.Group className="col-md-3 col-lg-6 col-xl-3 px-md-2">
         <Form.Label>Exp. Month</Form.Label>
         <Form.Control type="text" placeholder="MM"
           name="exp_month" value={creditCard.exp_month} onChange={changeCreditCard} maxLength="2"/>
         </Form.Group>
           
-        <Form.Group className="col-md-3 pl-md-2">
+        <Form.Group className="col-md-3 col-lg-6 col-xl-3 pl-md-2">
         <Form.Label>Exp. Year</Form.Label>
         <Form.Control type="text" placeholder="YY"
           name="exp_year" value={creditCard.exp_year} onChange={changeCreditCard} maxLength="2"/>
         </Form.Group>
-        <Form.Group className="col-md-4 mb-12">
+        <Form.Group className="col-md-4 col-lg-6 col-xl-4 mb-12">
         <Form.Label>Security Code</Form.Label>
         <Form.Control type="text" placeholder="CVV" maxLength="4" minLength="3"
           name="cvv" value={creditCard.cvv} onChange={changeCreditCard} />
