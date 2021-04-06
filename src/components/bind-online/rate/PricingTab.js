55e08603c3ca356f00a8ce2994f097b5b20d3995
johnathan guzman <jguzman@priscorp.net>
2021-04-06 12:57:05 -0500

    Added tooltip icon and content. Added a class in _custom.scss for darker text color. Inline css for testing, will remove.

--- src/components/bind-online/rate/PricingTab.js
+++ src/components/bind-online/rate/PricingTab.js
@@ -14,6 +14,10 @@ import { averageCoverageStrength } from '../../../services/rate-quality';
 import mixpanel                    from '../../../config/mixpanel'
 import history                     from '../../../history'
 
+import { AiOutlineInfoCircle } from 'react-icons/ai'
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import { Popover } from "react-bootstrap"
+
 function PricingTabs({ rate, quote, setShowEmailQuoteModal }) {
   const monthlyOption = monthlyPaymentOption(rate)
   const annualOption  = payInFullOption(rate)
@@ -39,10 +43,34 @@ function PricingTabs({ rate, quote, setShowEmailQuoteModal }) {
 
   let averageStrength = averageCoverageStrength(rate);
 
+  const tooltipData = "One of the major factors in your auto insurance rate is driving history. When updating your quote, we ran a Motor Vehicle Report (MVR) to analyze the driving history of all drivers included in the policy. If the report reveals violations or accidents for any of the drivers then your rate can increase. If you have questions about your MVR or want to learn more, please call us now at 844-358-5605 or chat online with a licensed agent to assist you in getting the best rate possible."
+
+
   return (
     <div className='bg-white shadow-lg rate-card-tabs'>
       <div className="rate-item-card">
-        <div className="title mb-2">Quote #{rate.id}</div>
+        <div className="mb-2 d-flex align-items-center justify-content-between">
+          <div className="title">Quote #{rate.id}</div> 
+          <div className="d-flex align-items-center">
+            <p className="mb-0 pr-2 text-medium-dark" style={{ color: "#767F7B" }}>Rate change?</p>
+            <OverlayTrigger
+              trigger="click"
+              key="bottom"
+              placement="bottom"
+              overlay={
+                <Popover id={`popover-positioned-bottom`} style={{ boxShadow: "2px 2px 10px 2px rgba(0,0,0,0.25)", border: "none" }}>
+                  <Popover.Content className="my-2">
+                  <Popover.Title className="mb-2 text-more-dark" as="h3" style={{ padding: 0, backgroundColor: "#fff", borderBottom: 0 }}>Why did my rate change?</Popover.Title>
+                  {tooltipData}
+                  </Popover.Content>
+                </Popover>
+              }
+            >
+              <AiOutlineInfoCircle style={{ height: "20px", width: "20px", color: "#BFC4C2" }}/>
+            </OverlayTrigger>
+          </div>
+        </div>
+        
         <div>As low as...</div>
         <div className="d-flex price-container mb-2">
           <p className="price-container__price quote-price display-1 mb-0">
