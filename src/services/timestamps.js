e3114582084e1d58efb0434b697c02ed4793155c
will ramirez <wramirez1980@gmail.com>
2021-04-08 15:06:55 -0500

    add new timestamp function to convert to unix timestamp
    add new timestamp function to convert to unix timestamp

--- src/services/timestamps.js
+++ src/services/timestamps.js
@@ -6,3 +6,5 @@ export default getDate;
 export const getTimestamp = (date) => dayjs(date, 'YYYY-MM-DD').toISOString()
 
 export const policyExpiry = (startDate, months) => dayjs(startDate, 'YYYY-MM-DD').add(`${months}`, 'month').toISOString()
+
+export const makeUnix = (date) => dayjs(date).unix();
