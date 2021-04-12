e5fbaf3a918b990149be14d7a94ad8b84434872b
johnathan guzman <jguzman@priscorp.net>
2021-04-12 10:57:39 -0500

    Changed text to InsureOnline

---
+++ src/components/main/PrivacyPolicy.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container, Row, Col }    from 'react-bootstrap';
+
+const PrivacyPolicy = () => {
+    return (
+        <Container className="pt-base">
+            <div className="shadow bg-white rounded">
+                <Row className="justify-content-center">
+                    <Col lg={6}>Hello world</Col>
+                </Row>
+            </div>
+        </Container>
+    )
+}
+
+export default PrivacyPolicy
