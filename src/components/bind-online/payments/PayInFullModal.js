087170e6c145ff77a1d2666cb8b6524992e1923b
johnathan guzman <jguzman@priscorp.net>
2021-04-21 10:52:59 -0500

    Testing chat script

--- src/components/bind-online/payments/PayInFullModal.js
+++ src/components/bind-online/payments/PayInFullModal.js
@@ -1,8 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Modal, Button, Row, Col }          from 'react-bootstrap';
 import { withTranslation }    from "react-i18next";
 
 const PayInFullModal = ({ t, show }) => {
+
+    useEffect(() => {
+        if (typeof window !== `undefined`) {
+
+            window.HFCHAT_CONFIG = {
+              EMBED_TOKEN: '999cd250-875a-11eb-9669-6fc7a3f61b09',
+              ASSETS_URL: `https://widget.happyfoxchat.com/v2/visitor`
+            }
+        }
+    })
 
     const [modalState, setModalState] = useState(show)
     
@@ -16,7 +26,7 @@ const PayInFullModal = ({ t, show }) => {
                             <p>{t("PayInFullModal.mainCopy")}</p>
                         </div>
                         <div className="my-4">
-                            <p>{t("PayInFullModal.agentCopy.web")}</p>
+                            <p onClick={() => <script async={true} src={`${window.HFCHAT_CONFIG.ASSETS_URL}/js/widget-loader.js`}></script>}>{t("PayInFullModal.agentCopy.web")}</p>
                             <p>{t("PayInFullModal.agentCopy.phone")}</p>
                             <p>{t("PayInFullModal.agentCopy.email")}</p>
                         </div>
