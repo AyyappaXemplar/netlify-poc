70cdafc449f1353ce283f9700a9350909e20397f
johnathan guzman <jguzman@priscorp.net>
2021-05-13 12:35:03 -0500

    Fixed bbb ui positioning

--- src/components/rate/Carrier.js
+++ src/components/rate/Carrier.js
@@ -15,19 +15,26 @@ export default function Carrier({ carrier }) {
 
   return (
     <>
-      <div className="d-flex mb-3 flex-column flex-md-row align-items-center">
-        <div style={{maxWidth: '200px'}}>
+      <div className="d-flex mb-3 flex-column flex-column flex-lg-row align-items-center">
+        <div className="pt-4 mr-4" >
           <img style={{width: '100%'}} src={`https://wi-sirius-production.nyc3.cdn.digitaloceanspaces.com/assets/carriers/logos/${carrier.tag.toLowerCase()}.png`} alt="carrier"/>
+          <div style={{ minWidth: "160px" }} className="d-none d-lg-flex align-items-center mr-3 pt-4">
+            <ReactStars count={5} value={4.5} size={24} color2={'#ffd700'} edit={false} half={true}/>
+            <span className="ml-2">9.5/10</span>
+          </div>
         </div>
-          <h4 className="px-md-5 pt-4">{carrier.name}</h4>
+          <div className="flex-column d-lg-flex">
+            <h4 className="pt-4 text-center text-md-left">{carrier.name}</h4>
+            <a className="d-none d-lg-flex pt-4" href={CARRIER_LINKS[carrier.tag]} target="_blank" rel="noopener noreferrer nofollow"><img src={bbbLogo} style={{ border: "0", width: "150px", height: "30px" }} alt="First Chicago Insurance Co. BBB Business Review" /></a>
+          </div>
       </div>
 
-      <div className="mb-3 d-sm-flex align-items-center">
-        <div className="d-flex align-items-center mr-3">
+      <div className="mb-3 d-flex flex-column flex-sm-row justify-content-center">
+        <div style={{ minWidth: "160px" }} className="d-flex d-lg-none align-items-center flex-row">
           <ReactStars count={5} value={4.5} size={24} color2={'#ffd700'} edit={false} half={true}/>
           <span className="ml-2">9.5/10</span>
         </div>
-        <a className="d-flex" href={CARRIER_LINKS[carrier.tag]} target="_blank" rel="noopener noreferrer nofollow"><img src={bbbLogo} style={{ border: "0", width: "150px", height: "30px" }} alt="First Chicago Insurance Co. BBB Business Review" /></a>
+        <a className="d-md-flex d-lg-none" href={CARRIER_LINKS[carrier.tag]} target="_blank" rel="noopener noreferrer nofollow"><img src={bbbLogo} className="b-none" style={{ width: "150px", height: "30px" }} alt="First Chicago Insurance Co. BBB Business Review" /></a>
       </div>
       <p className="text-med-dark">
         {carrier.description}
