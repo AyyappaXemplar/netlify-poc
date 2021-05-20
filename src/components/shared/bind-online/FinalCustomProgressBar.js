3407cee4c7cc3bc142a895062ca49309aa72a178
johnathan guzman <jguzman@priscorp.net>
2021-05-19 23:42:04 -0500

    Translations cleanup

--- src/components/shared/bind-online/FinalCustomProgressBar.js
+++ src/components/shared/bind-online/FinalCustomProgressBar.js
@@ -3,15 +3,15 @@ import { ProgressBar } from 'react-bootstrap';
 import { withTranslation } from 'react-i18next';
 import { finalProgressBarPercentages } from '../../../constants/progress-bar'
 
-function CustomProgressBar({ progress }) {
+function CustomProgressBar({ progress, t }) {
   const now = finalProgressBarPercentages[progress]
 
   return (
     <div className="custom-progress-bar my-2 my-lg-0 mx-auto">
       <div className='d-flex justify-content-between pb-2'>
-        <span className="small">Payments</span>
-        <span className="small">Signatures</span>
-        <span className="small">Confirmation</span>
+        <span className="small">{t("payments")}</span>
+        <span className="small">{t("signatures")}</span>
+        <span className="small">{t("confirmation")}</span>
       </div>
       <ProgressBar now={now} variant='primary' />
     </div>
