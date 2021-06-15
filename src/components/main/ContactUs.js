a174fe6188cfade27488ec7ad81b3ff65bf0429f
johnathan guzman <jguzman@priscorp.net>
2021-06-15 04:57:23 -0500

    Added error page events

--- src/components/main/ContactUs.js
+++ src/components/main/ContactUs.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import { withTranslation }     from 'react-i18next'
-
 import ContactCard from '../shared/ContactCard'
 import { Helmet } from 'react-helmet'
+import mixpanel from "../../config/mixpanel"
 
 function ContactUs({ t }) {
+
+  useEffect(() => {
+    mixpanel.track("Pageview", { "Page Title": "Contact Us Error Page" })
+  }, [])
+
   return (
     <Container className="py-4 container-rate-overview__inner">
       <Helmet>
