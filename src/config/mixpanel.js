9531cb67552648d7cc8c4f1a0f44ed8bd3684445
will ramirez <wramirez1980@gmail.com>
2021-06-21 11:48:43 -0500

    fix build error caused by linting

--- src/config/mixpanel.js
+++ src/config/mixpanel.js
@@ -21,11 +21,15 @@ const actions = {
     },
   },
 };
+
+
 // last touch utm tracking
 function getQueryParam(url, param) {
   // Expects a raw URL
-  // param = param.replace(/[[]/, "[").replace(/[]]/, "]");
-  var regexS = "[?&]" + param + "=([^&#]*)",
+// eslint-disable-next-line no-useless-escape, no-empty-character-class
+  param = param.replace(/[[]/, "\[").replace(/[]]/, "\]");
+  // eslint-disable-next-line no-useless-escape
+  var regexS = "[\?&]" + param + "=([^&#]*)",
       regex = new RegExp( regexS ),
       results = regex.exec(url);
   if (results === null || (results && typeof(results[1]) !== 'string' && results[1].length)) {
