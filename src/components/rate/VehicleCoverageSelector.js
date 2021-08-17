c7691a253d35bc63c9768d8bc6039af6178f1494
johnathanthedev <johnathanguzman17@gmail.com>
2021-08-17 18:01:20 -0500

    [WIP] Updating quote on coverage select

--- src/components/rate/VehicleCoverageSelector.js
+++ src/components/rate/VehicleCoverageSelector.js
@@ -1,10 +1,15 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { useSelector, useDispatch }   from 'react-redux'
 import { Nav }                        from 'react-bootstrap'
 
 import { coveragePackages }       from '../../constants/vehicle'
 import { updateVehicleCoverages } from '../../actions/vehicles'
 import { withTranslation }     from 'react-i18next'
+import { updateQuote } from "../../actions/quotes"
+import {
+  monthlyPaymentOption,
+  payInFullOption
+} from '../../services/payment-options';
 
 function VehicleCoverageSelector({ vehicle, t }) {
   const LABELS=[t("coverages.Basic"), t("coverages.Better"), t("coverages.Enhanced")]
@@ -15,16 +20,41 @@ function VehicleCoverageSelector({ vehicle, t }) {
     BETTER:    LABELS[2]
   }
 
-  const [selectedCoverage, setSelectedCoverage] = useState(vehicle.coverage_package_name)
   const updatingVehicleCoverage = useSelector(state => state.state.updatingVehicleCoverage)
+  const quote = useSelector(state => state.data.quote)
+  const all_rates = useSelector(state => state.data.rates)
+  const initial_rate = useSelector(state => state.data.rates[0])
+  const PAY_IN_FULL_LABEL = 'Pay In Full'
+  const MONTHLY_PAY_LABEL = 'Monthly'
+  const defaultActiveKey  = quote.pay_in_full ? PAY_IN_FULL_LABEL : MONTHLY_PAY_LABEL
+
+  const [selectedCoverage, setSelectedCoverage] = useState(vehicle.coverage_package_name)
+  // eslint-disable-next-line
+  const [activeTab, setActiveTab] = useState(defaultActiveKey)
+
   const dispatch = useDispatch()
 
+  const update_quote = useCallback(() => {
+  }, [])
+
   useEffect(() => {
+    // update_quote()
     if (updatingVehicleCoverage) return
     if (selectedCoverage !== vehicle.coverage_package_name) {
-      dispatch(updateVehicleCoverages(vehicle, selectedCoverage))
+      dispatch(updateVehicleCoverages(vehicle, selectedCoverage)).finally(() => {
+        const displayedPaymentOptions = () => {
+          return [monthlyPaymentOption(initial_rate), payInFullOption(initial_rate)]
+        }
+  
+        const quote_number = all_rates[0].id
+        const paymentOptions = displayedPaymentOptions()
+        const planCodeIndex = activeTab === MONTHLY_PAY_LABEL ? 0 : 1
+        const payment_plan_code = paymentOptions[planCodeIndex].plan_code
+    
+        dispatch(updateQuote({ ...quote, payment_plan_code, quote_number }))   
+      })
     }
-  }, [dispatch, vehicle, selectedCoverage, updatingVehicleCoverage])
+  }, [dispatch, vehicle, selectedCoverage, updatingVehicleCoverage, update_quote, activeTab, all_rates, initial_rate, quote])
 
   const handleSelect = (eventKey) => setSelectedCoverage(eventKey)
 
@@ -44,13 +74,20 @@ function VehicleCoverageSelector({ vehicle, t }) {
     <Nav variant="pills"
       activeKey={selectedCoverage}
       className="justify-content-center p-1 rounded bg-med-light mb-4"
-      onSelect={handleSelect}
+      onSelect={(e) => {
+        handleSelect(e)
+        // update_quote()
+      }}
+      onClick={update_quote}
     >
       { Object.values(coveragePackages).map((coverage, index) => (
         <Nav.Item className="flex-1" key={coverage} >
           <Nav.Link
             disabled={updatingVehicleCoverage}
-            onSelect={() => setSelectedCoverage(coverage)}
+            onSelect={() => {
+              setSelectedCoverage(coverage)
+              // update_quote()
+            }}
             className={`w-100 text-center ${updatingVehicleCoverage ? 'text-medium' : ''}`}
             eventKey={coverage}>
             {navLinkLabelDisplay(coverage)}
