c6b853d67f824d230f0fb342d2af69c2d135ef03
AyyappaXemplar <114500039+AyyappaXemplar@users.noreply.github.com>
2022-11-02 17:57:48 +0530

    Revert "INON-15: showing wrong age (#549)"
    This reverts commit 417f628d5f7fe581841fbcda3d78ca4a88de4af5.

--- src/services/driver-age.js
+++ src/services/driver-age.js
@@ -3,8 +3,8 @@ import * as dayjs from "dayjs"
 export function ageToDate(age) {
   let date = new Date()
   date.setFullYear(date.getFullYear() - age)
-  date.setMonth(date.getMonth())
-  date.setDate(date.getDate())
+  date.setMonth(0)
+  date.setDate(1)
   return date.toISOString().match(/\d{4}-\d{2}-\d{2}/)[0]
 }
 
@@ -22,16 +22,16 @@ export function getAge(date) {
 }
 
 export function displayBirthday(date) {
-  var arrDate = date.includes('/') ? date.split("/") : date.split("-");
+  var arrDate = date.split("-");
   return arrDate[1] + "/" + arrDate[2] + "/" + arrDate[0];
 }
 
 export function displayLinuxDate(linuxDate) {
-  var arrDate = linuxDate.includes('/') ? linuxDate.split("/") : linuxDate.split("-");
+  var arrDate = linuxDate.split("-");
   return arrDate[1] + "/" + arrDate[2] + "/" + arrDate[0];
 }
 
 export function formatBDayForAPI(bday) {
-  let date = new Date(`${bday} UTC`)
+  let date = new Date(bday)
   return date.toISOString().match(/\d{4}-\d{2}-\d{2}/)[0]
 }
